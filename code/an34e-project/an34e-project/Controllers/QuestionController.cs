2b405002aa13913270764622c663c1d3469296e8
Carlos Fernando dos Santos <fer.1972@hotmail.com>
2017-11-10 21:07:49 -0200

    Alterações

--- code/an34e-project/an34e-project/Controllers/QuestionController.cs
+++ code/an34e-project/an34e-project/Controllers/QuestionController.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using BancoDominio;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,9 +11,25 @@ namespace an34e_project.Controllers
 {
     public class QuestionController : Controller
     {
-        public ActionResult Index()
+        public string QueryQuestions(Int32 Level = 0)
         {
-            return View();
+            var strDb = ConfigurationManager.ConnectionStrings["db"].ConnectionString.ToString();
+
+            var conn = new SqlConnection(strDb);
+            conn.Open();
+
+            var cmd = new SqlCommand("Select question From Question Where level = " + Level , conn);
+
+            SqlDataReader dt = cmd.ExecuteReader();
+            string ret = "";
+
+            while (dt.Read()) {
+                ret = dt.GetString(0);
+            }
+
+            conn.Close();
+
+            return ret;
         }
     }
 }
---
+++ code/an34e-project/an34e-project/Models/Question.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace an34e_project.Models {
+    public class Question {
+        public Int32 Id { get; set; }
+        public String Quest { get; set; }
+        public Int32 Level { get; set; }
+        public Int32 RequiredLevel { get; set; }
+        public Boolean Removed { get; set; }
+    }
+}
