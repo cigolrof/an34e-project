c7298c877d8d87787c31c2d499ec19e83adf9949
Carlos Fernando dos Santos <fer.1972@hotmail.com>
2017-11-24 20:23:35 -0200

    Alterações em Customers

--- code/an34e-project/an34e-project/Models/Customer.cs
+++ code/an34e-project/an34e-project/Models/Customer.cs
@@ -23,16 +23,20 @@ namespace an34e_project.Models
             if (this.Id == 0) //insert
             {
                 conn.Open();
-                cmd = new SqlCommand("insert into users (login, password) values (@login, @senha)", conn);
-                cmd.Parameters.Add(new SqlParameter("@login", "aaaa") { DbType = DbType.String });
-                cmd.Parameters.Add(new SqlParameter("@senha", "ssssss") { DbType = DbType.String });
+                cmd = new SqlCommand("insert into customers (name, responsible, customer_since) values (@name, @responsible, @customer_since)", conn);
+                cmd.Parameters.Add(new SqlParameter("@name", Name) { DbType = DbType.String });
+                cmd.Parameters.Add(new SqlParameter("@responsible", Responsible) { DbType = DbType.String });
+                cmd.Parameters.Add(new SqlParameter("@customer_since", CustomerSince) { DbType = DbType.DateTime });
             }
             else //update
             {
                 conn.Open();
-                cmd = new SqlCommand("insert into users (login, password) values (@login, @senha)", conn);
-                cmd.Parameters.Add(new SqlParameter("@login", "aaaa") { DbType = DbType.String });
-                cmd.Parameters.Add(new SqlParameter("@senha", "ssssss") { DbType = DbType.String });
+                cmd = new SqlCommand("update customers set name = @name, responsible = @responsible, customer_since = @customer_since, removed = @removed where id = @id", conn);
+                cmd.Parameters.Add(new SqlParameter("@id", Id) { DbType = DbType.Int32 });
+                cmd.Parameters.Add(new SqlParameter("@name", Name) { DbType = DbType.String });
+                cmd.Parameters.Add(new SqlParameter("@responsible", Responsible) { DbType = DbType.String });
+                cmd.Parameters.Add(new SqlParameter("@customer_since", CustomerSince) { DbType = DbType.DateTime });
+                cmd.Parameters.Add(new SqlParameter("@removed", Removed) { DbType = DbType.Int32 });
 
             }
 
@@ -40,6 +44,30 @@ namespace an34e_project.Models
             conn.Close();
             return (rows > 0);
         }
+        public List<Customer> ListCustomer() 
+        {
+            var Db = ConfigurationManager.ConnectionStrings["db"].ConnectionString.ToString();
+            var connection = new SqlConnection(Db);
+            connection.Open();
+
+            var cmd = new SqlCommand("select t.id, t.name, t.responsible, t.customer_since, t.removed from customers t where t.removed = 0", connection);
+
+            SqlDataReader dt = cmd.ExecuteReader();
+            var lst = new List<Customer>();
+            if (dt.HasRows)
+                while (dt.Read()) {
+                    var obj = new Customer();
+                    obj.Id = dt.GetInt32(0);
+                    obj.Name = dt.GetString(1);
+                    obj.Responsible = dt.GetString(2);
+                    obj.CustomerSince = dt.GetDateTime(3);
+
+                    lst.Add(obj);
+                }
+            connection.Close();
+            return lst;
+        }
+
     }
 
 }
